9614e072ac144bf131ac5632f73d4b558b5532cb
Chirag Bansal <chiragb3005@gmail.com>
2026-03-17 12:28:50 +0530

    change of one data-type to other

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,23 @@
+// let score = "chirag";
+
+// console.log(typeof score)
+
+// let numberScore = Number(score)
+// console.log(numberScore)
+
+// number get convert 33
+// NaN when Number function used but it is not possible to convert into number
+// also seen boolean and null and undefined
+
+let isLoggedIn = "";
+let boolean = Boolean(isLoggedIn)
+
+console.log(typeof boolean);
+console.log(boolean);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "chirag" => true
+// null => false
+// undefined => false
