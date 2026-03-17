9b1fa47841ad45919a32b51cad66137bd8882f29
Chirag Bansal <chiragb3005@gmail.com>
2026-03-17 17:21:34 +0530

    comparison video done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>3);
+// no need to even run this low level thing 
+
+// console.log("2">1);
+// console.log("2">NaN);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// === => strict check  will also check data-type
+// == => loose check  will not check data-type
+
+console.log(2 == "2");
+console.log(2 === "2");
+
+// look above example
