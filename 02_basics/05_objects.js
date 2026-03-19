624f00093d781d2a0fbface93598d7767e2199a3
Chirag Bansal <chiragb3005@gmail.com>
2026-03-20 01:28:37 +0530

    last view over objects

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,11 @@
+// Destructuring
+
+const course = {
+    courseName : 'JS in hindi',
+    price: 1000,
+    instructor: 'hitesh'
+}
+// course.instructor
+
+const {instructor} = course
+console.log(instructor);
