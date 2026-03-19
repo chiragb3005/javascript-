336108b858e620cddbc005606219efafa51bf7e8
Chirag Bansal <chiragb3005@gmail.com>
2026-03-20 01:43:02 +0530

    added comments

--- 02_basics/05_objects.js
+++ 02_basics/05_objects.js
@@ -1,4 +1,6 @@
 // Destructuring
+// method so we dont have to call an object again n again 
+// will do same in React
 
 const course = {
     courseName : 'JS in hindi',
@@ -9,3 +11,6 @@ const course = {
 
 const {instructor} = course
 console.log(instructor);
+
+
+// later we have to go to API and debug them....... JSON
