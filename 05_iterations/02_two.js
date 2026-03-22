b9b59158afc50d86bdc135a870524cbb365beed1
Chirag Bansal <chiragb3005@gmail.com>
2026-03-23 02:59:06 +0530

    while & do-while done

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,22 @@
+let index = 1
+while (index<11) {
+    // console.log(`value of index is: ${index}`)
+    index = index+2
+}
+
+let myArray = ['flash', 'batman', 'sueprman']
+let arr = 0
+while(arr<myArray.length) {
+    // console.log(`value inside array: ${myArray[arr]}`)
+    arr++
+}
+
+// do-while loop
+
+let score = 11
+// loop will run atleast one time atleast
+
+do{
+    console.log(`score is: ${score}`)
+    score++
+} while (score<11)
