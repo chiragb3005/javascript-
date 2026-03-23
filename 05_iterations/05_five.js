ae297d223534a3b00e505ab3c8f84e3c8bed3320
Chirag Bansal <chiragb3005@gmail.com>
2026-03-23 18:28:13 +0530

    forEach loop used for mostly access objects inside array

---
+++ 05_iterations/05_five.js
@@ -0,0 +1,39 @@
+const coding = ['js', 'python', 'c++', 'ruby']
+coding.forEach( function (val){
+    // console.log(val)
+    // will print the array
+} )
+
+// Arrow function
+coding.forEach( (item) => {
+    // console.log(item)
+} )
+
+function printMe(item){
+    // console.log(item)
+}
+coding.forEach(printMe)
+// Dont call function only give the refrence 
+
+coding.forEach( (item, index, arr) => {
+    // console.log(item, index, arr)
+} )
+
+const myCoding = [
+    {
+        languageName: 'Javascript',
+        languageFilenName: 'js'
+    },
+    {
+        languageName: 'python',
+        languageFilenName: 'py'
+    },
+    {
+        languageName: 'c++',
+        languageFilenName: 'cpp'
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item)
+} )
